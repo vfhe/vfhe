73ae9477c04a380c9f25bd15005a75339d6326cd
Antonio Guimaraes <antonio.guimaraes@imdea.org>
2026-07-17 16:53:16 +0200

    Fix Format

--- modules/misc/python/src/vfhe/misc/__init__.py
+++ modules/misc/python/src/vfhe/misc/__init__.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # vfhe.misc subpackage.
-from .libvfhe import ffi, lib, libvfhe
 from . import dynamic_extensions
+from .libvfhe import ffi, lib, libvfhe
 
 __all__ = ["ffi", "lib", "libvfhe", "dynamic_extensions"]
